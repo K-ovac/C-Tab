de561ae6c22f972708695adb86e212a571d321d0
Kovac <biu.jay1@yandex.ru>
2026-09-10 21:00:07 +0300

    wip: Добавлены элементы в TokenInfoView

--- C-Tab/Scenes/TokenInfo/TokenInfoView.swift
+++ C-Tab/Scenes/TokenInfo/TokenInfoView.swift
@@ -8,10 +8,65 @@
 import SwiftUI
 
 struct TokenInfoView: View {
+    @State private var isExpanded: Bool = false
+    
     var body: some View {
         NavigationStack {
+            List {
+                warningTitle
+                    .listRowSeparator(.hidden)
+                tokenMetricsView
+                    .listRowSeparator(.hidden)
+                aboutToken
+                    .listRowSeparator(.hidden)
+            }
+            .navigationTitle("Coin Info")
+            .navigationBarTitleDisplayMode(.inline)
+            
+            .listStyle(.inset)
+        }
+    }
+}
+
+extension TokenInfoView {
+    private var warningTitle: some View {
+        Text(
+            """
+            *The token information and pricing data on this page are sourced from third parties (e.g, CoinMarketCap) and are for reference only. This app makes no warranties and does not constitute investment advice.
+            """
+        )
+        .foregroundStyle(.secondary)
+        .font(.caption)
+    }
+    
+    private var tokenMetricsView: some View {
+        Section() {
             TokenMetricsView()
         }
+    }
+    
+    private var aboutToken: some View {
+        Section {
+            VStack(alignment: .leading) {
+                Text(
+                    """
+                    Bitcoin (BTC) is a cryptocurrency launched in 2010. Users are able to generate BTC through the process of mining. Bitcoin has a current supply of 20,082,421. The last known price of Bitcoin is 77,335.98832106 USD and is down -1.77 over the last 24 hours. It is currently trading on 12736 active market(s) with $30,757,481,184.37 traded over the last 24 hours.
+                    """
+                )
+                .lineLimit(isExpanded ? nil : 3)
+                
+                Button(
+                    isExpanded ? "Hide" : "Show More"
+                ) {
+                    isExpanded.toggle()
+                }
+                .foregroundStyle(.secondary)
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        } header: {
+            Text("What is Bitcoin?")
+        }
+        
     }
 }
 

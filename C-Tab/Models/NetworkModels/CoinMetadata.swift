eac98401df2ca826ed6ba4d429510af7f1dec4a1
Kovac <biu.jay1@yandex.ru>
2026-09-12 21:46:07 +0300

    fix(model): обновлена модель CoinMуtadata

--- C-Tab/Models/NetworkModels/CoinMetadata.swift
+++ C-Tab/Models/NetworkModels/CoinMetadata.swift
@@ -9,18 +9,21 @@ struct CoinMetadata: Codable, Identifiable {
     let id: String
     let name: String
     let symbol: String
-    let desctiption: CoinDescription
+    let description: CoinDescription
     let image: CoinImage
-    let marketCapRank: Int
     let marketData: CoinMarketData
-    let priceChangePercentage24h: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case symbol
+        case description
+        case image
+        case marketData = "market_data"
+    }
 }
 
 struct CoinDescription: Codable {
-    let description: CoinDescriptionLanguage
-}
-
-struct CoinDescriptionLanguage: Codable {
     let en: String
     let ru: String
     let zh: String
@@ -32,6 +35,14 @@ struct CoinImage: Codable {
 
 struct CoinMarketData: Codable {
     let currentPrice: CoinCurrentPrice
+    let marketCapRank: Int
+    let priceChangePercentage24h: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case currentPrice = "current_price"
+        case marketCapRank = "market_cap_rank"
+        case priceChangePercentage24h = "price_change_percentage_24h"
+    }
 }
 
 struct CoinCurrentPrice: Codable {
